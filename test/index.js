96ea104600896cba5e8f9401b2ab9c1fee8881de
Webb Peng <webb.png@gmail.com>
2016-12-21 00:16:17 +0800

    update test command

--- test/index.js
+++ test/index.js
@@ -11,51 +11,50 @@ const commitMessage = 'test commit';
 
 const delay = (t = 1) => new Promise((done) => setTimeout(done, t * 1000));
 
-const start = async () => {
-	await exec(`rm -rf ${cwd}`);
-	await exec(`mkdir ${cwd}`);
-	await run('rm -rf .git');
-	await run('git init .');
-	await run('touch a.txt');
-	await run('touch b.txt');
-	await run('touch c.txt');
-	await run('git status -s');
+describe('git-add-cli', async () => {
+	it('🌚 ', async () => {
+		await exec(`rm -rf ${cwd}`);
+		await exec(`mkdir ${cwd}`);
+		await run('rm -rf .git');
+		await run('git init .');
+		await run('touch a.txt');
+		await run('touch b.txt');
+		await run('touch c.txt');
+		await run('git status -s');
 
-	const child = spawn(resolve('bin/git-add'), {
-		...options,
-		stdio: 'pipe',
+		const child = spawn(resolve('bin/git-add'), {
+			...options,
+			stdio: 'pipe',
+		});
+
+		child.stdin.setEncoding('utf-8');
+
+		// child.stdout.on('data', (data) => {
+		// 	const info = data.toString().trim();
+		// 	info && console.log(info);
+		// });
+
+		await delay();
+		child.stdin.write('a');
+
+		await delay();
+		child.stdin.write('\n');
+
+		await delay();
+		child.stdin.write('test commit');
+
+		await delay();
+
+		child.stdin.end();
+
+		await delay();
+
+		const [log] = await run('git log');
+
+		assert(log.trim().endsWith(commitMessage));
+
+		await exec(`rm -rf ${cwd}`);
 	});
 
-	child.stdin.setEncoding('utf-8');
-
-	// child.stdout.on('data', (data) => {
-	// 	const info = data.toString().trim();
-	// 	info && console.log(info);
-	// });
-
-	await delay();
-	child.stdin.write('a');
-
-	await delay();
-	child.stdin.write('\n');
-
-	await delay();
-	child.stdin.write('test commit');
-
-	await delay();
-
-	child.stdin.end();
-
-	await delay();
-
-	const [log] = await run('git log');
-
-	assert(log.trim().endsWith(commitMessage));
-
-	await exec(`rm -rf ${cwd}`);
-};
-
-start()
-	.catch((err) => console.error(err.message))
-	.then(() => exec(`rm -rf ${cwd}`))
-;
+	after(() => exec(`rm -rf ${cwd}`));
+});
